2f58f127a54c0a6de2d221a6f50b21630610a2f6
mary <mary@gmail.com>
2019-09-28 17:52:11 +0100

    made arrow click

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -7,8 +7,7 @@ import $ from "jquery";
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
-	text = `Femi Gbajabiamila (born June 25, 1962, surname also spelled Gbaja-Biamila and Gbajabiamila) is a Nigerian lawyer,>All Progressives Congress Leader, and House Leader of Nigeria's 8th House of Representatives. Gbaja Biamila attended Igbobi College in Yaba, Lagos, King William's College on the Isle of Man, United Kingdom, Atlanta's John Marshall Law School, United States, and the University of Lagos in Lagos. Gbaja Biamila was elected to the House in 2003, and re-elected in 2007.
-		He represents the Surulere I constituency of Lagos State in the House of Representatives. He is the House's Majority Leader.Gbaja Biamila has criticized members of Congress for switching parties. He suggested that many voters don't have access to the information to make choices based on every individual stance, and therefore are sometimes only voting for candidates based on their party alignment. He criticized floppers with this in mind, saying the effect "cannot be anything but negative.`;
+	
 
   constructor() { }
 
